b805a60bb6855736725bfc5de24a37a8a129a39c
wangtongmeng <tongmengwang@163.com>
2021-09-16 00:02:44 +0800

    项目搭建

---
+++ website/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <v-button>按钮</v-button>
+        <v-icon>图标</v-icon>
+    </div>
+</template>
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+    setup() {
+        
+    },
+})
+</script>
---
+++ website/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+import vue3UI from 'vue3-ui'
+
+// 创建应用 并使用组件库
+createApp(App).use(vue3UI).mount('#app')
