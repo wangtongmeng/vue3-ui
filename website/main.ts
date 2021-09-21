3cfc6c4deccd3a2a5af6098c624c2ee88f7625a0
wangtongmeng <tongmengwang@163.com>
2021-09-21 21:06:25 +0800

    组件打包

--- website/main.ts
+++ website/main.ts
@@ -1,8 +1,10 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-import vue3UI from 'vue3-ui'
-import '@vue3-ui/theme-chalk/index.scss'
+// import vue3UI from 'vue3-ui'
+// import vue3UI from '../lib/index.js'
+import vue3UI from '../lib/index.esm.js'
+import '@vue3-ui/theme-chalk/src/index.scss'
 
 // 创建应用 并使用组件库
 createApp(App).use(vue3UI).mount('#app')
